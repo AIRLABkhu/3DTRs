df5f85b4ab4c8e3537a7516a968676a6addc2a7d
Yu Geunhyeok <nda111@naver.com>
2022-12-19 10:31:42 +0900

    Update model/module/__init__.py: SPConv, WinAttn

--- PVT/model/module/__init__.py
+++ PVT/model/module/__init__.py
@@ -1,2 +1,5 @@
 from model.module.voxelization import Voxelization
 from model.module.devoxelization import Devoxelization
+from spconv3d_v1 import SparseConv3d as SparseConv3d_v1
+from spconv3d_v2 import SparseConv3d as SparseConv3d_v2
+from window_attention import WindowAttention
